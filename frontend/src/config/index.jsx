6949ddee722758a096882d364aca5264ba372f69
Sourabh Tiware <sourabhtiware1737@gmail.com>
2025-09-08 00:42:42 +0530

    Frontend Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const BASE_URL = "http://localhost:9090";
-export const BASE_URL = "https://pro-connect-linkedin-clone.onrender.com:9090";
+export const BASE_URL = "https://pro-connect-linkedin-clone.onrender.com";
 
 const clientServer = axios.create({
   baseURL: BASE_URL,
