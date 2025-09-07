8483c99fe742a2181e9cbfe1f06f4e429a3476cc
Sourabh Tiware <sourabhtiware1737@gmail.com>
2025-09-08 01:05:04 +0530

    Update ClientServer

--- frontend/src/utils/clientServer.jsx
+++ frontend/src/utils/clientServer.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:9090",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   withCredentials: true
 });
