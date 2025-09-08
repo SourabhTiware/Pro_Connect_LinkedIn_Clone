d5083239e0c266f04bfee9d11d1026c6a81141c7
Sourabh Tiware <sourabhtiware1737@gmail.com>
2025-09-08 11:44:09 +0530

    Some changes in ClientServer

--- frontend/src/utils/clientServer.jsx
+++ frontend/src/utils/clientServer.jsx
@@ -1,18 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
 
-const clientServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 10000,
-  withCredentials: true
-});
+// const clientServer = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+//   timeout: 10000,
+//   withCredentials: true
+// });
 
-// optional: request interceptor to add token
-clientServer.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (err) => Promise.reject(err));
+// // optional: request interceptor to add token
+// clientServer.interceptors.request.use((config) => {
+//   if (typeof window !== "undefined") {
+//     const token = localStorage.getItem("token");
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// }, (err) => Promise.reject(err));
 
-export default clientServer;
+// export default clientServer;
